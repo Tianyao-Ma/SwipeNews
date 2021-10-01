0b75032deda63fbc416f461e6a61ec6c376a5487
Tianyao Ma <tianyao.ma333@gmail.com>
2021-10-01 15:54:10 -0400

    integrated home database func with rest MVVM model

--- app/src/main/java/com/tianyaoma/tinnews/repository/NewsRepository.java
+++ app/src/main/java/com/tianyaoma/tinnews/repository/NewsRepository.java
@@ -1,10 +1,14 @@
 package com.tianyaoma.tinnews.repository;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import com.tianyaoma.tinnews.TinNewsApplication;
+import com.tianyaoma.tinnews.database.TinNewsDatabase;
+import com.tianyaoma.tinnews.model.Article;
 import com.tianyaoma.tinnews.model.NewsResponse;
 import com.tianyaoma.tinnews.network.NewsApi;
 import com.tianyaoma.tinnews.network.RetrofitClient;
@@ -15,8 +19,10 @@ import retrofit2.Response;
 
 public class NewsRepository {
     private final NewsApi newsApi;
+    private final TinNewsDatabase database;
     public NewsRepository(Context context) {
         newsApi = RetrofitClient.newInstance(context).create(NewsApi.class);
+        database = TinNewsApplication.getDatabase();
     }
     public LiveData<NewsResponse> getTopHeadlines(String country) {
         MutableLiveData<NewsResponse> topHeadLinesLiveData = new MutableLiveData<>();
@@ -38,6 +44,12 @@ public class NewsRepository {
         return topHeadLinesLiveData;
     }
 
+    public LiveData<Boolean> favoriteArticle(Article article) {
+        MutableLiveData<Boolean> resultLiveData = new MutableLiveData<>();
+        new FavoriteAsyncTask(database, resultLiveData).execute(article);
+        return resultLiveData;
+    }
+
     public LiveData<NewsResponse> searchNews(String query) {
         MutableLiveData<NewsResponse> everyThingLiveData = new MutableLiveData<>();
         newsApi.getEverything(query, 40).enqueue(new Callback<NewsResponse>() {
@@ -56,5 +68,31 @@ public class NewsRepository {
             }
         });
         return everyThingLiveData;
+    }
+    private static class FavoriteAsyncTask extends AsyncTask<Article, Void, Boolean> {
+
+        private final TinNewsDatabase database;
+        private final MutableLiveData<Boolean> liveData;
+
+        private FavoriteAsyncTask(TinNewsDatabase database, MutableLiveData<Boolean> liveData) {
+            this.database = database;
+            this.liveData = liveData;
+        }
+//Everything inside doInBackground would be executed on a separate background thread.
+        @Override
+        protected Boolean doInBackground(Article... articles) {
+            Article article = articles[0];
+            try {
+                database.articleDao().saveArticle(article);
+            } catch (Exception e) {
+                return false;
+            }
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success) {
+            liveData.setValue(success);
+        }
     }
 }
